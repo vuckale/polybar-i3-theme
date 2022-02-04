39f2964dd57613db25d3df8950ec59af522bb887
vuckale <56733276+vuckale@users.noreply.github.com>
2022-02-05 00:53:06 +0100

    modify xset_q script to disply status with color

--- scripts/xset_q.sh
+++ scripts/xset_q.sh
@@ -5,13 +5,13 @@ CAPS_LOCK_STATUS=$( echo $QUERY | grep "Caps Lock" | cut -d' ' -f 18 )
 NUM_LOCK_STATUS=$( echo $QUERY | grep "Num Lock" | cut -d' ' -f 22 )
 
 if [[ "$CAPS_LOCK_STATUS" == "on" ]]; then
-    CAPS_LOCK_STATUS="󰘲 on"
+    CAPS_LOCK_STATUS="%{F$ALERT}󰘲%{F-}"
 else
     CAPS_LOCK_STATUS=""
 fi
 
 if [[ "$NUM_LOCK_STATUS" == "off" ]]; then
-    NUM_LOCK_STATUS="󰎠 off"
+    NUM_LOCK_STATUS="%{F$ALERT}󰎠%{F-}"
 else
     NUM_LOCK_STATUS=""
 fi
