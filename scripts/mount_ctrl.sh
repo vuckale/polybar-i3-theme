8b7c919a2cf0f1df9d82814593a184c652c21785
vuckale <56733276+vuckale@users.noreply.github.com>
2022-02-05 00:35:42 +0100

    modify mount module to only display count of mounted drives

--- scripts/mount_ctrl.sh
+++ scripts/mount_ctrl.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
+unset DRIVES
+while IFS= read -r LINE; do
+    DRIVES+=("${LINE}")
+done < <( lsblk -r | grep /media | cut -d' ' -f 7,8 | cut -d'/' -f 4 && lsblk -r | grep /run | cut -d' ' -f 7,8 | cut -d'/' -f 4 )
+count=$( echo -e "${#DRIVES[@]}\c" )
+
 get_mounted_drive_info(){
-    unset DRIVES
-    while IFS= read -r LINE; do
-        DRIVES+=("${LINE}")
-    done < <( lsblk -r | grep /media | cut -d' ' -f 7,8 | cut -d'/' -f 4 && lsblk -r | grep /run | cut -d' ' -f 7,8 | cut -d'/' -f 4 )
-    count=$( echo -e "${#DRIVES[@]}\c" )
     if [ "$count" = "0" ]; then
 	    sep=$( echo -e "\c" )
     else
@@ -13,22 +15,37 @@ get_mounted_drive_info(){
     echo -e "%{A1:gnome-disks:}󰋊$sep%{A}\c"
     for drive in ${DRIVES[@]}; do
         name=$(lsblk -r | grep -F "$drive" | cut -d' '  -f 1)
-        udiskctl=$(echo "udisksctl unmount -b /dev/$name && notify-send \"mount_ctrl.sh\" \"unmounted $drive\" && sleep 3 && udisksctl power-off -b /dev/$name && notify-send \"mount_ctrl.sh\" \"powered-off $drive\"")
+        udiskctl=$( echo "udisksctl unmount -b /dev/$name && notify-send \"mount_ctrl.sh\" \"unmounted $drive\" && sleep 3 && udisksctl power-off -b /dev/$name && notify-send \"mount_ctrl.sh\" \"powered-off $drive\"" )
         echo -e "%{F$FOREGROUND_ALT}󱊟%{F-} %{T7}%{F$FOREGROUND}$drive%{F-}%{T-} %{A1:$udiskctl:}%{F$FOREGROUND_ALT}󰤁%{F-}%{A} | \c"
     done
     printf "\n"
 }
 
+get_mounted_drive_count(){
+    echo -e "%{A1:gnome-disks:}󰋊$sep%{A} | \c"
+    if [[ "$count" -gt "0" ]]; then
+	    echo -e "%{F$FOREGROUND_ALT}󱊟%{F-} %{T7}$count%{T-}\c"
+    fi
+}
+
+usage(){
+    if [ ! "$(which udisksctl)" = "" ]; then
+        return 0
+    else
+        # install udisks2
+        echo "$(date) ${PWD##*/}/`basename "$0"`: no udisks2 installed" >> ../log.txt
+        # install libnotify-bin for norify-send notifications
+        echo "X"
+        return 1
+    fi
+}
+
 case "$1" in
     --list)
-        if [ ! "$(which udisksctl)" = "" ]; then
-            get_mounted_drive_info
-        else
-            # install udisks2
-            echo "$(date) ${PWD##*/}/`basename "$0"`: no udisks2 installed" >> ../log.txt
-            # install libnotify-bin for norify-send notifications
-            echo "X"
-        fi
+        usage && get_mounted_drive_info
+        ;;
+    --count)
+        usage && get_mounted_drive_count
         ;;
 esac
 
